d0c16f8e56d94ccaa03ab0097f495a91f9c9bef8
tramhuuducvn <tramhuuduc.vn@gmail.com>
2022-11-15 00:01:31 +0700

    saved changes!!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="draw_text">
+        <svg width="350" height="40">
+          <text
+            x="40"
+            y="30"
+            fill="#000"
+            stroke="#c64242"
+            id="text-logo"
+            stroke-width="2"
+            font-size="24"
+          >
+            HAPPY BIRTHDAY TO YOU!
+          </text>
+        </svg>
+        <svg width="300" height="100">
+          <text
+            x="100"
+            y="40"
+            fill="#000"
+            stroke="#c64242"
+            id="text-logo"
+            stroke-width="2"
+            font-size="30"
+          >
+            18.11 - Pek Ut
+          </text>
+        </svg>
+      </div>
     </div>
   );
 }
